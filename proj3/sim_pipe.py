06c806012b1a2dfd687108f45635db36e937e086
tripplerizz <hectormrizo@gmail.com>
2020-12-01 21:07:06 -0500

    changing memory grabs

--- proj3/sim_pipe.py
+++ proj3/sim_pipe.py
@@ -76,14 +76,14 @@ def i_type_instr(opcode, instr, state):
     arg1 = binToNum(instr[13:16],0)
     arg2 = binToNum(instr[16:32],1)
     if op_type[opcode] == 'lw':  
-        state.reg[arg1] = state.mem[state.reg[arg0] + arg2 ]  
+        state.reg[arg1] = state.dataMem[state.reg[arg0] + arg2 ]  
         return
     if op_type[opcode] == 'beq':
         if (state.reg[arg0] - state.reg[arg1]) == 0:
             state.pc += arg2
             return
     if op_type[opcode] == 'sw':
-        state.mem[state.reg[arg0] + arg2] = state.reg[arg1]  
+        state.dataMem[state.reg[arg0] + arg2] = state.reg[arg1]  
 def o_type_instr(opcode, instr, state):
     if op_type[opcode] == 'halt':
         global done
@@ -114,7 +114,7 @@ def readInstr(instr, state):
         return
 
     return 0
-def printInstruction(instr):
+def getInstruction(instr):
     instr_str = numToBin(instr)
     opcode_str = op_type[binToNum(instr_str[:10], 0)]
     result = ""
@@ -123,25 +123,18 @@ def printInstruction(instr):
         arg1 = binToNum(instr_str[13:16],0)
         arg2 = binToNum(instr_str[29:32],0)
         result = "{0} {0} {0} {0}".format(opcode_str, arg0, arg1, arg2)
-        print(result)
-        return
     if instr_type[opcode_str] == 'O':
         result = "{0}".format(opcode_str)
-        print(result)
-        return
     if instr_type[opcode_str] == 'I':
         arg0 = binToNum(instr_str[10:13],0)
         arg1 = binToNum(instr_str[13:16],0)
         arg2 = binToNum(instr_str[16:32],1)
         result = "{0} {0} {0} {0}".format(opcode_str, arg0, arg1, arg2)
-        print(result)
-        return
     if instr_type[opcode_str] == 'J':
         arg0 = binToNum(instr_str[10:13],0)
         arg1 = binToNum(instr_str[13:16],0)
         result = "{0} {0} {0} ".format(opcode_str, arg0, arg1)
-        print(result)
-        return
+    return result
 
 def printState(state):
     print("\n@@@\nstate before cycle {num} starts\n".format(num =state.cycles))
@@ -156,28 +149,28 @@ def printState(state):
 	    print("\t\treg[ {num1} ] {num2}\n".format(num1 = index, num2 = val))
     print("\tIFID:\n")
     print("\t\tinstruction ")
-    printInstruction(state.IFID.instr)
+    print(getInstruction(state.IFID.instr))
     print("\t\tpcPlus1 {num}\n".format(num = state.IFID.pcPlus1))
     print("\tIDEX:\n")
     print("\t\tinstruction ")
-    printInstruction(state.IDEX.instr)
+    print(getInstruction(state.IDEX.instr))
     print("\t\tpcPlus1 {num}\n".format(num = state.IDEX.pcPlus1))
     print("\t\treadRegA {num}\n".format(num = state.IDEX.readRegA))
     print("\t\treadRegB {num}\n".format(num = state.IDEX.readRegB))
     print("\t\toffset {num}\n".format(num = state.IDEX.offset))
     print("\tEXMEM:\n")
     print("\t\tinstruction ")
-    printInstruction(state.EXMEM.instr)
+    print(getInstruction(state.EXMEM.instr))
     print("\t\tbranchTarget {num}\n".format(num = state.EXMEM.branchTarget))
     print("\t\taluResult {num}\n".format(num = state.EXMEM.aluResult))
     print("\t\treadRegB {num}\n".format(num = state.EXMEM.readRegB))
     print("\tMEMWB:\n")
     print("\t\tinstruction ")
-    printInstruction( state.MEMWB.instr)
+    print(getInstruction( state.MEMWB.instr))
     print("\t\twriteData {num}\n".format(num = state.MEMWB.writeData))
     print("\tWBEND:\n")
     print("\t\tinstruction ")
-    printInstruction(state.WBEND.instr)
+    print(getInstruction(state.WBEND.instr))
     print("\t\twriteData {num}\n".format(num = state.WBEND.writeData))
 
 #------------------------------------------------------------------------
@@ -189,11 +182,14 @@ if len(sys.argv) < 2:
 state = stateStruct()
 with open(sys.argv[1], 'r') as assembly:
     for line in assembly:
-        state.instrMem[state.numMemory] = (int(line))
+        state.instrMem[state.numMemory] = getInstruction(int(line))
         state.dataMem[state.numMemory] = (int(line))
-        print("mem[{num}]={num2}".format(num = state.numMemory, num2 = state.mem[state.numMemory]))
+        print("mem[{num}]={num2}".format(num = state.numMemory, num2 = state.dataMem[state.numMemory]))
         state.numMemory +=1
-print("\n")
+print("\tinstruction memory:")
+for index, val in enumerate(state.instrMem):
+    print("\t\tinstrMem[ {num} ]={num2}".format(num = index, num2 = val))
+
 
 count = 0
 while(True):
@@ -205,7 +201,7 @@ while(True):
     printState(state)
     if done:
         break
-    instr = numToBin(state.mem[state.pc])
+    instr = numToBin(state.dataMem[state.pc])
     readInstr(instr, state)
     state.pc +=1
     count += 1
